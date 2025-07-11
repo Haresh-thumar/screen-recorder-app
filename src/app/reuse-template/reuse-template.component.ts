10f226aa31b029cc55575e8ac4c093d8ced0c659
Haresh-thumar <hareshofficial95@gmail.com>
2025-07-11 22:06:12 +0530

    delete all components spact files

--- src/app/reuse-template/reuse-template.component.ts
+++ src/app/reuse-template/reuse-template.component.ts
@@ -1,11 +1,10 @@
-import { NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-reuse-template',
   standalone: true,
-  imports: [NgTemplateOutlet],
+  imports: [],
   templateUrl: './reuse-template.component.html',
   styleUrl: './reuse-template.component.scss',
 })
-export class ReuseTemplateComponent {}
+export class ReuseTemplateComponent { }
